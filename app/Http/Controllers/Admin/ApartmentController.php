fa49e45c3d4eb14437e9f114f3f76e640c0a5400
Luca10992 <lucalillo.99.1@gmail.com>
2024-05-04 12:56:45 +0200

    feat. add crud apartments

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Apartment;
 use App\Http\Controllers\Controller;
 
+use App\Models\Service;
+use App\Models\Sponsor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -18,12 +20,11 @@ class ApartmentController extends Controller
     public function index()
     {
         $id = Auth::id();
-        $apartments = Apartment::select(['title_desc', 'n_rooms', 'n_bathrooms', 'n_beds', 'square_mts', 'img', 'visible', 'latitude', 'longitude'])
-            ->where('user_id', $id)->paginate(10);
+        $apartments = Apartment::where('user_id', $id)->paginate(10);
 
             $apiKey = "J3iuAWIFiXr0BqrC4gh2RHMmzjR7mdUt";
             $addresses = [];
-            foreach ($apartments as $key=>$apartment) {
+            foreach ($apartments as $apartment) {
                 $address_path = "https://api.tomtom.com/search/2/reverseGeocode/{$apartment->latitude},{$apartment->longitude}.json?key={$apiKey}";
                 $address_json = file_get_contents($address_path);
                 $address_obj = json_decode($address_json);
@@ -40,7 +41,9 @@ class ApartmentController extends Controller
      */
     public function create()
     {
-        //
+        $apartment = new Apartment;
+        $services = Service::all();
+        return view('admin.apartments.form', compact('apartment', 'services'));
     }
 
     /**
@@ -51,7 +54,26 @@ class ApartmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $apiKey = "J3iuAWIFiXr0BqrC4gh2RHMmzjR7mdUt";
+        $request->validated();
+        $data = $request->all();
+        
+        $apartment = new apartment();
+        $apartment->fill($data);
+        $address_path = str_replace("%20", " ", $apartment->address);
+        $coordinate_path = "https://api.tomtom.com/search/2/geocode/{$apartment->n_address}%20{$address_path},%20{$apartment->city},%20{$apartment->country}.json?key={$apiKey}";
+        $coordinate_json = file_get_contents($coordinate_path);
+        $coordinate_obj = json_decode($coordinate_json);
+        array_push($apartment->latitude, $coordinate_obj->results->position->lat);
+        array_push($apartment->longitude, $coordinate_obj->results->position->lon);
+        $apartment->save();
+
+
+        if (array_key_exists('services', $data)) {
+            $apartment->services()->attach($data['services']);
+        }
+
+        return redirect()->route('admin.apartments.show', $apartment);
     }
 
     /**
@@ -62,7 +84,7 @@ class ApartmentController extends Controller
      */
     public function show(Apartment $apartment)
     {
-        //
+        return view('admin.apartments.show', compact('apartment'));
     }
 
     /**
@@ -73,7 +95,9 @@ class ApartmentController extends Controller
      */
     public function edit(Apartment $apartment)
     {
-        //
+        $services = Service::all();
+        // $apartment_service_id = $apartment->services->pluck('id')->toArray();
+        return view('admin.apartments.form', compact('apartment', 'services'));
     }
 
     /**
@@ -85,7 +109,16 @@ class ApartmentController extends Controller
      */
     public function update(Request $request, Apartment $apartment)
     {
-        //
+        $request->validated();
+        $data = $request->all();
+        
+        $apartment->update($data);
+        
+        if (array_key_exists('services', $data)) {
+            $apartment->services()->attach($data['services']);
+        }
+
+        return redirect()->route('admin.apartments.show', $apartment);
     }
 
     /**
@@ -96,6 +129,7 @@ class ApartmentController extends Controller
      */
     public function destroy(Apartment $apartment)
     {
-        //
+        $apartment->delete();
+        return redirect()->route('admin.apartments.index');
     }
 }
