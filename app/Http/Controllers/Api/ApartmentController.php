a6c43d5a647dc65c8386df8b85cea02fd60af2c4
Luca10992 <lucalillo.99.1@gmail.com>
2024-05-09 13:18:52 +0200

    feat. add search filter function

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -27,28 +27,72 @@ class ApartmentController extends Controller
             'success' => true,
         ]);
     }
-    public function index()
+    public function index(Request $request)
     {
-        $apartments = Apartment::where('visible', true )->with(['services:id,name,logo', 'sponsors:id,name,duration,price'])->paginate(20);
+        $query = Apartment::query();
+
+        if($request->has('beds') && $request['beds'] != 0) {
+            $query->where('n_beds', '>=', $request['beds']);
+        }
+
+        if($request->has('rooms') && $request['rooms'] != 0) {
+            $query->where('n_rooms', '>=', $request['rooms']);
+        }
+
+        if($request->has('services') && $request['services'] != []) {
+            $services = $request['services'];
+            $query->whereHas('services', function ($q) use ($services) {
+                $q->whereIn('service_id', $services);
+            }, '=', count($services));
+        }
+
+        if($request->has('address') && $request['address'] != "") {
+            $apiKey = "J3iuAWIFiXr0BqrC4gh2RHMmzjR7mdUt";
+            
+            $address_path = str_replace(" ", "%20", $request['address']);
+            $coordinate_path = "https://api.tomtom.com/search/2/geocode/{$address_path}.json?key={$apiKey}";
+            $coordinate_json = file_get_contents($coordinate_path);
+            $coordinate_obj = json_decode($coordinate_json);
+            $latitude = $coordinate_obj->results[0]->position->lat;
+            $longitude = $coordinate_obj->results[0]->position->lon;
+
+            $query->whereRaw('ST_Distance( POINT(apartments.longitude, apartments.latitude),POINT(' . $longitude . ',' . $latitude . ')) < ' . $request['range'] / 100);
+        }
         
-        foreach ($apartments as $apartment) {
-            if (str_starts_with($apartment->img, 'img')) {
-                $apartment->img = asset($apartment->img);
-            } elseif (str_starts_with($apartment->img, 'uploads')) {
-                $apartment->img = asset('storage/' . $apartment->img);
-            } else {
-                $apartment->img = "https://placehold.co/600x400";
+        $apartments = $query->where('visible', true )->with(['services:id,name,logo', 'sponsors:id,name,duration,price'])->get()->toArray();
+        
+        $sponsoredApartments = [];
+        
+        foreach ($apartments as $index => $apartment) {
+            if ($apartment['sponsors'] != []) {
+                unset($apartments[$index]);
+                array_push($sponsoredApartments, $apartment);
             }
         }
 
+        foreach($sponsoredApartments as $sponsoredApartment) {
+            array_unshift($apartments, $sponsoredApartment);
+        }
+        foreach ($apartments as $apartment) {
+            if (str_starts_with($apartment['img'], 'img')) {
+                $apartment['img'] = asset($apartment['img']);
+            } elseif (str_starts_with($apartment['img'], 'uploads')) {
+                $apartment['img'] = asset('storage/' . $apartment['img']);
+            } elseif ($apartment['img'] = '') {
+                // ******Debug********
+                $apartment['img'] = "https://placehold.co/600x400";
+            }
+            // ******Aggiungere address in array apartment************
+        }
+
         return response()->json([
-            'result' => $apartments,
             'success' => true,
+            'result' => $apartments,
         ]);
     }
     public function show($slug)
     {
-        $apartment = apartment::where('slug', $slug)->with(['services:id,name,logo', 'sponsors:id,name,duration,price'])->first();
+        $apartment = apartment::where('slug', $slug)->with(['users:id,name,surname,date_of_birth,email','services:id,name,logo', 'sponsors:id,name,duration,price'])->first();
         if (empty($apartment)) {
             return response()->json([
                 'message' => 'Appartamento non trovato',
