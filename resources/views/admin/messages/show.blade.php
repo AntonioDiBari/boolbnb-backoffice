f1810be8bd8bb1966e955f92d59a08ff01001db8
FrancescoCesari97 <freedus97@gmail.com>
2024-05-13 16:11:31 +0200

    adjust layout index and show of messages

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -1,12 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container my-3">
-    <strong>Email: </strong> {{ (reset($messages_filter))['email'] }} <br />
-    @foreach ($messages_filter as $message)
-    <strong>Body: </strong> {{ $message['body'] }} <br />
-    <strong>Sent at: </strong> {{ $message['sent'] }} <br />
-    @endforeach
-</div>
-@endsection
+    <div class="container my-5 d-flex justify-content-center">
+        <div class="message-container  p-3 bg-white rounded shadow">
+            <div class="d-flex justify-content-between pb-5">
+                <div>
 
+                    <strong>From: </strong> {{ reset($messages_filter)['email'] }} <br />
+                    @foreach ($messages_filter as $message)
+                </div>
+                <div>
+
+                    <span class="fw-bold">date: </span><span class="fw-light ">{{ $message['sent'] }}</span> <br />
+                </div>
+
+            </div>
+
+            <div class="bg-body-secondary p-3 bg-white rounded ">
+                {{ $message['body'] }} <br />
+
+            </div>
+            @endforeach
+
+        </div>
+    </div>
+@endsection
